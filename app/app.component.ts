04c3c122cb276aaa1932208a234ef28a450ae237
周献杭 <jozhouxian@gmail.com>
2018-06-12 22:40:45 +0800

    0612
    step1 for dynamic component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,45 @@
-import { Component } from '@angular/core';
-import { User} from "./auth-form/auth-form.interface";
+import { Component, ViewContainerRef, AfterContentInit, ViewChild, ComponentFactoryResolver } from '@angular/core';
+
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+import { User } from './auth-form/auth-form.interface';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <auth-form (submitted)="login($event)">
+      <!--auth-form (submitted)="login($event)">
         <h3>Login User</h3>
         <button type="submit">Login</button>
         <auth-remember (checked)="rememberUser($event)"></auth-remember>
-      </auth-form>
+      </auth-form -->
+
+      <!-- dynamic component -->
+      <div #entry></div>
     </div>
   `
 })
-export class AppComponent {
-  createUser(user: User){
+export class AppComponent implements AfterContentInit {
+
+  @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
+
+  constructor(private resolver: ComponentFactoryResolver) { }
+
+  ngAfterContentInit() {
+    const autoFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
+    const component = this.entry.createComponent(autoFormFactory);
+  }
+
+  createUser(user: User) {
     console.log('auth-form create user ', user);
   }
 
-  login(user: User){
+  login(user: User) {
     console.log('auth-form login user ', user);
   }
 
-  rememberUser(evt: any){
-    console.log('auth-form message: ',evt);
+  rememberUser(evt: any) {
+    console.log('auth-form message: ', evt);
   }
 }
